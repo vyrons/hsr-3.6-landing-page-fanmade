c1fc4f4a217d4eef4ffdf2ed1e389833586ba988
vyrons <w.yuhao.gti@gmail.com>
2025-09-16 10:11:30 +0700

    add hero and button feature

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Hero from './components/Hero'
+import About from './components/About'
+
+const App = () => {
+  return (
+    <main className='relative min-h-screen w-screen overflow-x-hidden'>
+      <Hero/>
+      <About/>
+      
+    </main>
+  )
+}
+
+export default App
