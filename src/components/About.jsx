fd3014a307602a2fbe983533506db7863ec73b97
vyrons <w.yuhao.gti@gmail.com>
2025-09-16 16:49:12 +0700

    add about section

---
+++ src/components/About.jsx
@@ -0,0 +1,59 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
+gsap.registerPlugin(ScrollTrigger);
+
+const About = () => {
+  useGSAP(() => {
+    const clipAnimation = gsap.timeline({
+      scrollTrigger: {
+        trigger: "#clip",
+        start: "center center",
+        end: "+=800 center",
+        scrub: 0.5,
+        pin: true,
+        pinSpacing: true,
+      },
+    });
+
+    clipAnimation.to(".mask-clip-path", {
+      width: "100vw",
+      height: "100vh",
+      borderRadius: 0,
+    });
+  });
+
+  return (
+    <div id="about" className="min-h-screen w-screen">
+      <div className="relative mb-8 mt-36 flex flex-col items-center gap-5">
+        <h2 className="font-general text-sm uppercase md:text-[10px]">
+          {" "}
+          Welcome to Honkai Star Rail
+        </h2>
+
+        <div className="mt-5 text-center text-4xl uppercase leading-[0.8] md:text-[6rem]">
+          STEP INT<b>O</b> THE ENDLESS <br />
+          NIGHT OF <b>A</b>MPHOREUS
+        </div>
+
+        <div className="about-subtext">
+          <p>Explore Amphoreus</p>
+          <p>a world shrouded in eternal night.</p>
+          <p>The adventure begins in Version 3.6.</p>
+        </div>
+      </div>
+
+      <div className="h-dvh w-screen" id="clip">
+        <div className="mask-clip-path about-image">
+          <img
+            src="img/about.webp"
+            alt="Background"
+            className="absolute left-0 top-0 size-full object-cover"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
